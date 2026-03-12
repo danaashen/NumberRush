7e197466f83b39caa53ea407857d4667c68b0ac4
dakosh <230107034@sdu.edu.kz>
2026-03-12 16:17:54 +0500

    Initial Commit

---
+++ NumberRush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberRush
+//
+//  Created by Dana Ashen on 12.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
